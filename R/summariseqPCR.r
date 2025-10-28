ccc1a86c76e8d6d932c1392e045712fcb7f9630b
Dionne Argyropoulos <dionne.argy@gmail.com>
2025-10-28 20:50:16 +1100

    removed cv calculation

--- R/summariseqPCR.r
+++ R/summariseqPCR.r
@@ -18,7 +18,6 @@ summariseqPCR <- function(df){
       min  = if (all(is.na(vals))) NA_real_ else min(vals, na.rm = TRUE),
       max  = if (all(is.na(vals))) NA_real_ else max(vals, na.rm = TRUE),
       sd   = if (all(is.na(vals))) NA_real_ else sd(vals, na.rm = TRUE),
-      cv   = sd * 100,
       difference  = max - min,
       range_score = dplyr::case_when(
         all(is.na(vals))        ~ NA_character_,
